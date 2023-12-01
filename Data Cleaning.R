133a87133e008a36fcaf97ca74839ab4e61dc26a
pddiii <peterdepaul124@gmail.com>
2023-12-01 12:19:21 -0800

    Added: HOF Player Voting Awards to consider for feature engineering Fixed spelling errors

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -8,6 +8,36 @@ data(Batting)
 data(Fielding)
 # Load the Pitching Data
 data(Pitching)
+# Load the Player Personal Information
+data(People)
+# Load the Season Awards Data
+data(AwardsPlayers)
+# Players who have appeared on the Hall of Fame Ballots
+# and received votes
+hof_players <- HallOfFame %>% 
+  mutate(pct_vote = round((votes / ballots), 4), .after = votes) %>% 
+  filter(category == "Player", !is.na(pct_vote))
+# Attach the HOF players to their respective Player Information
+hof_players <- hof_players %>% 
+  inner_join(People, by = "playerID") %>% 
+  select(-c(11:22), -deathDate, -birthDate)
+# Awards for Consideration
+awards <- c("Triple Crown", "Most Valuable Player", "World Series MVP", 
+            "Cy Young Award", "Rolaids Relief Man Award", 
+            "Outstanding DH Award", "Reliever of the Year Award")
+# 
+AwardsPlayers %>% 
+  # Fix Spelling error in the Awards Data
+  mutate(awardID = ifelse(awardID == "SIlver Slugger", "Silver Slugger", 
+                           awardID)) %>%
+  filter(awardID %in% awards) %>% 
+  group_by(playerID, awardID) %>% 
+  summarise(num_awards = n()) %>% 
+  arrange(desc(num_awards))
+  
+  
+Fielding
+
 # Append the batting statistics for players included on Hall
 # of Fame ballots
 hof_batting <- HallOfFame %>% 
