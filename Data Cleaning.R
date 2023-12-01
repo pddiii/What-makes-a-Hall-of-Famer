ceccdfd1580274e9826fdce1c78bfb5341c4a309
pddiii <peterdepaul124@gmail.com>
2023-12-01 14:12:53 -0800

    Fixed Spacing for readability Added: - Career Fielding Stats - Added various Awards (and their counts by player)

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -12,31 +12,55 @@ data(Pitching)
 data(People)
 # Load the Season Awards Data
 data(AwardsPlayers)
+
 # Players who have appeared on the Hall of Fame Ballots
 # and received votes
 hof_players <- HallOfFame %>% 
   mutate(pct_vote = round((votes / ballots), 4), .after = votes) %>% 
   filter(category == "Player", !is.na(pct_vote))
+
 # Attach the HOF players to their respective Player Information
 hof_players <- hof_players %>% 
   inner_join(People, by = "playerID") %>% 
   select(-c(11:22), -deathDate, -birthDate)
+
+# Attach the HOF players (batters) to their primary position
+# First Subset the Fielding Data to our desired parameters
+Fielding <- Fielding %>% 
+  select(-c(2:5), -c(14:18)) %>% 
+  filter(POS != "P") # Pitchers have separate Data Set, no need to include
+
+# Redefine the numeric variable as Career Position Stats
+career_fielding <- Fielding %>%
+  group_by(playerID, POS) %>%
+  summarise_if(is.numeric, sum)
+
+career_fielding %>% 
+  filter(playerID %in% unique(hof_players$playerID),
+         G / sum(G) >= 0.5)
+
 # Awards for Consideration
 awards <- c("Triple Crown", "Most Valuable Player", "World Series MVP", 
-            "Cy Young Award", "Rolaids Relief Man Award", 
-            "Outstanding DH Award", "Reliever of the Year Award")
-# 
-AwardsPlayers %>% 
-  # Fix Spelling error in the Awards Data
+            "Cy Young Award", "Rolaids Relief Man Award", "Gold Glove",
+            "Outstanding DH Award", "Reliever of the Year Award", 
+            "Silver Slugger")
+
+# Fix Spelling error in the Awards Data
+AwardsPlayers <- AwardsPlayers %>% 
   mutate(awardID = ifelse(awardID == "SIlver Slugger", "Silver Slugger", 
-                           awardID)) %>%
+                           awardID))
+
+# Number of Awards by player, and awardID
+num_awards <- AwardsPlayers %>% 
   filter(awardID %in% awards) %>% 
   group_by(playerID, awardID) %>% 
-  summarise(num_awards = n()) %>% 
-  arrange(desc(num_awards))
+  summarise(num_awards = n())
   
-  
-Fielding
+pivot_wider(data = num_awards,
+            id_cols = playerID,
+            names_from = awardID,
+            values_from = num_awards,
+            values_fill = 0)
 
 # Append the batting statistics for players included on Hall
 # of Fame ballots
@@ -47,6 +71,7 @@ hof_batting <- HallOfFame %>%
         inner_join(Batting, by = "playerID") %>% 
         group_by(playerID) %>% 
         summarise(across(-c(1:5), sum)) # Summarise for Career Stats
+
 # Append the pitching statistics for players on Hall of Fame
 # ballots
 HallOfFame %>% 
